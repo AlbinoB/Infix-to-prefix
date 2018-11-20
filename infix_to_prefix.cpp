95c42213a4d13a2c24bd21737c08785992dd2805
AlbinoB <albinobraganza21@gmail.com>
2018-11-20 23:44:58 +0530

    convert fns not working

--- infix_to_prefix.cpp
+++ infix_to_prefix.cpp
@@ -68,5 +68,109 @@ int main()
 		}
 
 void convert(char *copyarrstring){
+    char symbolstack[20],outputstack[20];
+    int i=0,topsymbolstack=-1,topoutputstack=-1;
+    while(copyarrstring[i]!='\0')
+    {
+        if(isempty(topoutputstack)==1)
+                    {
+                        cout<<"\n output:";
+                        cout<<"Stack underflow!!!";
+                    }
+                    else
+                    {
+                        cout<<"\n output:";
+                       display(outputstack);
+                    }
+                    if(isempty(topsymbolstack)==1)
+                    {
+                       cout<<"\n symbol:";
+                        cout<<"Stack underflow!!!";
+                    }
+                    else
+                    {
+                         cout<<"\n symbol:";
+                         display(symbolstack);
+                    }
+        cout<<"\n i:"<<i;
+       if((int)copyarrstring[i]==')'||(int)copyarrstring[i]==']'||(int)copyarrstring[i]=='}')
+        {
+                 if(isempty(topsymbolstack)==1&&i==0)
+                {
+                    cout<<"\n error!!! Wrong input";
+                    break;
+                }else
+                {
+                   if((int)copyarrstring[i]==')')
+                    {
+                        cout<<"\n ''''''"<<topoutputstack;
+                        char popped,tempstack[20];
+                        int toptempstack=0;
+                        while((int)outputstack[topoutputstack]!='(')
+                        {
+
+                            popped=pop(outputstack,topoutputstack);
+                            topoutputstack--;
+                            toptempstack=push(tempstack,toptempstack,popped);
+                        }
+                        topoutputstack--;
+                        popped=pop(symbolstack,topsymbolstack);
+                        topsymbolstack--;
+                        topoutputstack=push(outputstack,topoutputstack,popped);
+                        for(int j=toptempstack;j>=0;j--)
+                        {
+                            popped=pop(tempstack,toptempstack);
+                            toptempstack--;
+                            outputstack[topoutputstack++]=popped;
+                        }
+                        outputstack[topoutputstack+1]='\0';
+                    }
+                    else
+                    {
+                        if((int)copyarrstring[i]==']'||(int)copyarrstring[i]=='}')
+                        {
+                            char popped;
+                             while((int)symbolstack[topsymbolstack]!=(int)copyarrstring[i]-2)
+                            {
+
+                                popped=pop(symbolstack,topsymbolstack);
+                                topsymbolstack--;
+                                topoutputstack=push(outputstack,topoutputstack,popped);
+                                outputstack[topoutputstack+1]='\0';
+                                }
+                                popped=pop(symbolstack,topsymbolstack);
+                                topsymbolstack--;
+                            }
+                      }
+
+
+                    }
+                    i++;
+        }
+        else
+        {
+            if((int)copyarrstring[i]>64&&(int)copyarrstring[i]<91||(int)copyarrstring[i]>96&&(int)copyarrstring[i]<123||(int)copyarrstring[i]=='('||(int)copyarrstring[i]=='['||(int)copyarrstring[i]=='{')
+        {
+            topoutputstack=push(outputstack,topoutputstack,copyarrstring[i++]);
+            outputstack[topoutputstack+1]='\0';
+
+        }else
+        {
+            if((int)copyarrstring[i]=='*'||(int)copyarrstring[i]=='/'||(int)copyarrstring[i]=='+'||(int)copyarrstring[i]=='-')
+            {
+                topsymbolstack=push(symbolstack,topsymbolstack,copyarrstring[i++]);
+                symbolstack[topsymbolstack+1]='\0';
+            }
+            else
+            {
+                    cout<<"\n wrong input!!!";
+                    break;
+            }
+
+        }
+
+    }
+        }
+strcpy(copyarrstring,outputstack);
 
 }
